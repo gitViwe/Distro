f67dc0652d996fe884bc1010d2207589fc624a86
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-06-17 08:29:34 +0200

    Added constant for errors

---
+++ Distro/gitViwe.Shared/Constant/AuthorizationError.cs
@@ -0,0 +1,27 @@
+﻿namespace gitViwe.Shared;
+
+/// <summary>
+/// Provides error messages for authorization errors
+/// </summary>
+public static class AuthorizationError
+{
+    /// <summary>
+    /// Token has expired
+    /// </summary>
+    public const string ExpiredToken = "The Token has expired.";
+
+    /// <summary>
+    /// Not authorized to access
+    /// </summary>
+    public const string Unauthorized = "You are not Authorized.";
+
+    /// <summary>
+    /// Unhandled server error
+    /// </summary>
+    public const string InternalServerError = "An unhandled error has occurred.";
+
+    /// <summary>
+    /// Not authorized to access
+    /// </summary>
+    public const string Forbidden = "You are not authorized to access this resource.";
+}
