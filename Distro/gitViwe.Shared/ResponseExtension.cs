9e520e0509949667a099eea3f28351983988d084
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-07-02 07:36:01 +0200

    added wrapper extensions

---
+++ Distro/gitViwe.Shared/ResponseExtension.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace gitViwe.Shared
+{
+    /// <summary>
+    /// Provides wrapper extensions for the the <see cref="HttpResponseMessage"/>
+    /// </summary>
+    public static class ResponseExtension
+    {
+        /// <summary>
+        /// Process the HTTP response message into the wrapper class <see cref="Response"/>
+        /// </summary>
+        /// <typeparam name="TData">The data type returned from the response</typeparam>
+        /// <param name="response">The HTTP response message from the API</param>
+        /// <returns>The content of an HTTP response message as a <see cref="Response{TData}"/> model</returns>
+        public static async Task<IResponse<TData>> ToResultAsync<TData>(this HttpResponseMessage response) where TData : class, new()
+        {
+            // get the response as a string
+            var responseAsString = await response.Content.ReadAsStringAsync();
+
+            // deserialize response into a 'Result' wrapper class
+            var responseObject = JsonSerializer.Deserialize<Response<TData>>(responseAsString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+
+            // return the 'Result' wrapper class
+            return responseObject;
+        }
+
+        /// <summary>
+        /// Process the HTTP response message into the wrapper class <see cref="Response"/>
+        /// </summary>
+        /// <param name="response">The HTTP response message from the API</param>
+        /// <returns>The content of an HTTP response message as a <see cref="Response"/> model</returns>
+        public static async Task<IResponse> ToResultAsync(this HttpResponseMessage response)
+        {
+            // get the response as a string
+            var responseAsString = await response.Content.ReadAsStringAsync();
+
+            // deserialize response into a 'Result' wrapper class
+            var responseObject = JsonSerializer.Deserialize<Response>(responseAsString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+
+            // return the 'Result' wrapper class
+            return responseObject;
+        }
+    }
+}
