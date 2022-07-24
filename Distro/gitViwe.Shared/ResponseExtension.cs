ef6895ffbee09c3108df6c388f1392f6f9a6e66c
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-07-24 19:03:02 +0200

    Add ToPaginatedResponseAsync extension method

--- Distro/gitViwe.Shared/ResponseExtension.cs
+++ Distro/gitViwe.Shared/ResponseExtension.cs
@@ -13,8 +13,30 @@ namespace gitViwe.Shared
         /// </summary>
         /// <typeparam name="TData">The data type returned from the response</typeparam>
         /// <param name="response">The HTTP response message from the API</param>
+        /// <returns>The content of an HTTP response message as a <see cref="PaginatedResponse{TData}"/> model</returns>
+        public static async Task<PaginatedResponse<TData>> ToPaginatedResponseAsync<TData>(this HttpResponseMessage response) where TData : class, new()
+        {
+            // get the response as a string
+            var responseAsString = await response.Content.ReadAsStringAsync();
+
+            // deserialize response into a 'Result' wrapper class
+            var responseObject = JsonSerializer.Deserialize<PaginatedResponse<TData>>(responseAsString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+
+            // return the 'Result' wrapper class
+            return responseObject;
+        }
+
+        /// <summary>
+        /// Process the HTTP response message into the wrapper class <see cref="Response"/>
+        /// </summary>
+        /// <typeparam name="TData">The data type returned from the response</typeparam>
+        /// <param name="response">The HTTP response message from the API</param>
         /// <returns>The content of an HTTP response message as a <see cref="Response{TData}"/> model</returns>
-        public static async Task<IResponse<TData>> ToResultAsync<TData>(this HttpResponseMessage response) where TData : class, new()
+        public static async Task<IResponse<TData>> ToResponseAsync<TData>(this HttpResponseMessage response) where TData : class, new()
         {
             // get the response as a string
             var responseAsString = await response.Content.ReadAsStringAsync();
@@ -35,7 +57,7 @@ namespace gitViwe.Shared
         /// </summary>
         /// <param name="response">The HTTP response message from the API</param>
         /// <returns>The content of an HTTP response message as a <see cref="Response"/> model</returns>
-        public static async Task<IResponse> ToResultAsync(this HttpResponseMessage response)
+        public static async Task<IResponse> ToResponseAsync(this HttpResponseMessage response)
         {
             // get the response as a string
             var responseAsString = await response.Content.ReadAsStringAsync();
