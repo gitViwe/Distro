338c7ca97541c0e6cb514b85677c1e14a3ac41e6
Viwe Nkepu <viwe.nkepu@hotmail.com>
2023-04-09 14:06:21 +0200

    Up version to 1.4.3
    Added utility class 'FileSizeFormatter'

---
+++ Distro/gitViwe.Shared/Utility/FileSizeFormatter.cs
@@ -0,0 +1,27 @@
+﻿namespace gitViwe.Shared.Utility;
+
+/// <summary>
+/// Provides a converter to format file sizes
+/// </summary>
+public static class FileSizeFormatter
+{
+    private static readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+
+    /// <summary>
+    /// Formats the bytes size to the nearest larger format<br></br>
+    /// 1024 Bytes  ---> 1 KB
+    /// </summary>
+    /// <param name="bytes">The size in bytes</param>
+    /// <returns>A formatted byte size string</returns>
+    public static string FormatSize(long bytes)
+    {
+        int counter = 0;
+        decimal number = bytes;
+        while (Math.Round(number / 1024) >= 1)
+        {
+            number = number / 1024;
+            counter++;
+        }
+        return string.Format("{0:n1}{1}", number, suffixes[counter]);
+    }
+}
