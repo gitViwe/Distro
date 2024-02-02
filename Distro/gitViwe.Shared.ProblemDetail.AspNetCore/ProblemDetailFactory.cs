edb308d72c760e4eb72672e5306e3edaa8e5ac24
Viwe Nkepu <viwe.nkepu@hotmail.com>
2024-02-02 16:39:11 +0200

    add more ProblemDetailFactory methods

--- Distro/gitViwe.Shared.ProblemDetail.AspNetCore/ProblemDetailFactory.cs
+++ Distro/gitViwe.Shared.ProblemDetail.AspNetCore/ProblemDetailFactory.cs
@@ -30,6 +30,25 @@ public static class ProblemDetailFactory
     }
 
     /// <summary>
+    /// Creates a <see cref="IResult"/> instance that configures defaults for <br></br>
+    /// <see cref="DefaultProblemDetails.Status"/><br></br>
+    /// <see cref="DefaultProblemDetails.Type"/><br></br>
+    /// <see cref="DefaultProblemDetails.Title"/><br></br>
+    /// <see cref="HttpResponse.ContentType"/><br></br>
+    /// <see cref="HttpResponse.StatusCode"/>
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext" />.</param>
+    /// <param name="statusCode">The value for <see cref="DefaultProblemDetails.Status"/>.</param>
+    /// <param name="detail">The value for <see cref="DefaultProblemDetails.Detail" />.</param>
+    /// <returns>A <see cref="IResult"/> class</returns>
+    public static IResult CreateProblemResult(HttpContext context, int statusCode, string? detail = null)
+    {
+        IDefaultProblemDetails problem = CreateProblemDetails(context, statusCode, detail);
+
+        return Results.Problem(problem.Detail, problem.Instance, problem.Status, problem.Title, problem.Type, problem.Extensions);
+    }
+
+    /// <summary>
     /// Creates a <see cref="DefaultProblemDetails"/> instance that configures defaults for <br></br>
     /// <see cref="DefaultProblemDetails.Status"/><br></br>
     /// <see cref="DefaultProblemDetails.Type"/><br></br>
@@ -51,6 +70,26 @@ public static class ProblemDetailFactory
     }
 
     /// <summary>
+    /// Creates a <see cref="IResult"/> instance that configures defaults for <br></br>
+    /// <see cref="DefaultProblemDetails.Status"/><br></br>
+    /// <see cref="DefaultProblemDetails.Type"/><br></br>
+    /// <see cref="DefaultProblemDetails.Title"/><br></br>
+    /// <see cref="HttpResponse.ContentType"/><br></br>
+    /// <see cref="HttpResponse.StatusCode"/>
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext" />.</param>
+    /// <param name="statusCode">The value for <see cref="DefaultProblemDetails.Status"/>.</param>
+    /// <param name="extensions">The object extension associated with this instance of <see cref="DefaultProblemDetails"/></param>
+    /// <param name="detail">The value for <see cref="DefaultProblemDetails.Detail" />.</param>
+    /// <returns>A <see cref="IResult"/> class</returns>
+    public static IResult CreateProblemResult(HttpContext context, int statusCode, IDictionary<string, object?> extensions, string? detail = null)
+    {
+        IDefaultProblemDetails problem = CreateProblemDetails(context, statusCode, extensions, detail);
+
+        return Results.Problem(problem.Detail, problem.Instance, problem.Status, problem.Title, problem.Type, problem.Extensions);
+    }
+
+    /// <summary>
     /// Creates a <see cref="DefaultValidationProblemDetails"/> instance that configures defaults for <br></br>
     /// <see cref="DefaultProblemDetails.Status"/><br></br>
     /// <see cref="DefaultProblemDetails.Type"/><br></br>
@@ -69,5 +108,25 @@ public static class ProblemDetailFactory
         context.Response.StatusCode = statusCode;
 
         return BaseProblemDetailFactory.CreateValidationProblemDetails(statusCode, context.Request.Path, errors, detail);
+    }
+
+    /// <summary>
+    /// Creates a <see cref="IResult"/> instance that configures defaults for <br></br>
+    /// <see cref="DefaultProblemDetails.Status"/><br></br>
+    /// <see cref="DefaultProblemDetails.Type"/><br></br>
+    /// <see cref="DefaultProblemDetails.Title"/><br></br>
+    /// <see cref="HttpResponse.ContentType"/><br></br>
+    /// <see cref="HttpResponse.StatusCode"/>
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext" />.</param>
+    /// <param name="statusCode">The value for <see cref="DefaultProblemDetails.Status"/>.</param>
+    /// <param name="errors">The errors associated with this instance of <see cref="DefaultValidationProblemDetails"/></param>
+    /// <param name="detail">The value for <see cref="DefaultProblemDetails.Detail" />.</param>
+    /// <returns>A <see cref="IResult"/> class</returns>
+    public static IResult CreateValidationProblemResult(HttpContext context, int statusCode, IDictionary<string, string[]> errors, string? detail = null)
+    {
+        IValidationProblemDetails problem = CreateValidationProblemDetails(context, statusCode, errors, detail);
+
+        return Results.ValidationProblem(problem.Errors, problem.Detail, problem.Instance, problem.Status, problem.Title, problem.Type, problem.Extensions);
     }
 }
