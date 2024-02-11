c56cd03fa0e1c7b45881514e9137bf96d609c976
Viwe Nkepu <viwe.nkepu@hotmail.com>
2024-02-11 17:12:59 +0200

    resolve DI container values

--- Distro/gitViwe.Shared.Cache/Startup.cs
+++ Distro/gitViwe.Shared.Cache/Startup.cs
@@ -14,11 +14,14 @@ public static class Startup
     /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
     public static IServiceCollection AddGitViweRedisCache(this IServiceCollection services, Action<RedisDistributedCacheOption> options)
     {
+        RedisDistributedCacheOption def = new();
+        options(def);
+
         services.Configure(options)
             .AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = options.Configuration;
-                options.InstanceName = options.InstanceName;
+                options.Configuration = def.Configuration;
+                options.InstanceName = def.InstanceName;
             })
             .AddScoped<IRedisDistributedCache, DefaultRedisDistributedCache>()
             .AddOptionsWithValidateOnStart<RedisDistributedCacheOption, RedisDistributedCacheOptionValidator>("RedisDistributedCacheOption", options);
